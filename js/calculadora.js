effd98065230db331d7012f6011b07ba6f8f697e
Isabelle Almeida <isabellealmeida12@gmail.com>
2023-03-09 21:13:03 -0300

    projeto v2

--- js/calculadora.js
+++ js/calculadora.js
@@ -31,7 +31,7 @@ function multiplicar() {
   const numero1 = parseFloat(document.getElementById("numero1Mult").value);
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
   // evite mexer no código acima!
-mult = (numero1 - numero2)
+mult = (numero1 * numero2)
 
 
 
@@ -45,12 +45,12 @@ function dividir() {
   const numero1 = parseFloat(document.getElementById("numero1Div").value);
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
   // evite mexer no código acima!
-
+ divis= (numero1 / numero2)
 
 
 
   
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = divis;
 }
